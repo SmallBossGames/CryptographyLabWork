9ea3ddbd69d7edfc59fdb2ff48ef03b7805f2c8d
Алексей Гардер <aleksey_garder@outlook.com>
2019-11-05 19:58:03 +0700

    Add CRC16, CRC32 and CRC64

--- СryptographyLabWork/Algorithms/CalculationCRC.cs
+++ СryptographyLabWork/Algorithms/CalculationCRC.cs
@@ -9,34 +9,96 @@ namespace СryptographyLabWork.Algorithms
 {
     static class CalculationCRC
     {
-        public static ulong CalculationCRC64(byte[] sourceBytes, ulong CRCBase)
+        public static ulong CalculationCRC64(byte[] sourceBytes)
         {
-            NormalizeArray(ref sourceBytes, sizeof(ulong));
-            
-            var value = BitConverter.ToUInt64(sourceBytes, 0);
+            var crcTable = BuildReverseCRC64Table(0xC96C5795D7870F42);
 
-            for (int i = 0; i < sourceBytes.Length - sizeof(ulong); i++)
+            var crc = 0xffffffffffffffffUL;
+
+            foreach (var item in sourceBytes)
             {
-                var hotLoadIndex = sizeof(ulong) + i;
-                var hotLoadByte = (ulong)sourceBytes[hotLoadIndex];
-
-                for (int j = sizeof(byte) - 1; j >= 0 ; j--)
-                {
-                    value = (value << 1) | (hotLoadByte >> j) & 1UL;
-                    if ((value & 1UL<<63) != 0)
-                    {
-                        value ^= CRCBase;
-                    }
-                }
+                crc = crcTable[(crc ^ item) & 0xFFUL] ^ (crc >> 8);
             }
-            return value;
+            return crc ^ 0xffffffffffffffffUL;
         }
 
-        private static void NormalizeArray(ref byte[] source, int minCount)
+        public static uint CalculationCRC32(byte[] sourceBytes)
         {
-            if (source.Length >= minCount)
-                return;
-            Array.Resize(ref source, minCount);
+            var crcTable = BuildReverseCRC32Table(0xEDB88320U);
+
+            var crc = 0xffffffffU;
+
+            foreach (var item in sourceBytes)
+            {
+                crc = crcTable[(crc ^ item) & 0xFF] ^ (crc >> 8);
+            }
+            return crc ^ 0xFFFFFFFFU;
+        }
+
+        public static ushort CalculationCRC16(byte[] sourceBytes)
+        {
+            var crcTable = BuildReverseCRC16Table(0xA001);
+
+            ushort crc = 0xffff;
+
+            foreach (var item in sourceBytes)
+            {
+                crc = (ushort)(crcTable[(crc ^ item) & 0xFF] ^ (crc >> 8));
+            }
+            return (ushort)(crc ^ 0xffff);
+        }
+
+        private static uint[] BuildReverseCRC32Table(uint crcRevBase)
+        {
+            var crcTable = new uint[256];
+
+            for (uint i = 0; i < crcTable.Length; i++)
+            {
+                var tempCrc = i;
+                for (int j = 0; j < 8; j++)
+                {
+                    tempCrc = (tempCrc & 1U) != 0 ? (tempCrc >> 1) ^ crcRevBase : tempCrc >> 1;
+                }
+
+                crcTable[i] = tempCrc;
+            }
+
+            return crcTable;
+        }
+
+        private static ulong[] BuildReverseCRC64Table(ulong crcRevBase)
+        {
+            var crcTable = new ulong[256];
+
+            for (int i = 0; i < crcTable.Length; i++)
+            {
+                var tempCrc = (ulong)i;
+                for (int j = 0; j < 8; j++)
+                {
+                    tempCrc = (tempCrc & 1U) != 0 ? (tempCrc >> 1) ^ crcRevBase : tempCrc >> 1;
+                }
+
+                crcTable[i] = tempCrc;
+            }
+
+            return crcTable;
+        }
+
+        private static ushort[] BuildReverseCRC16Table(ushort crcRevBase)
+        {
+            var crcTable = new ushort[256];
+
+            for (ushort i = 0; i < crcTable.Length; i++)
+            {
+                var tempCrc = i;
+                for (int j = 0; j < 8; j++)
+                {
+                    tempCrc = (ushort)((tempCrc & 1) != 0 ? (tempCrc >> 1) ^ crcRevBase : tempCrc >> 1);
+                }
+                crcTable[i] = tempCrc;
+            }
+
+            return crcTable;
         }
     }
 }
